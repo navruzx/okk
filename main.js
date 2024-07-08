ae47cfc683afd5cec80867c3e081e4e3b2d2b1ce
navruzx <nnavruz095@gmail.com>
2024-07-08 16:05:17 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,99 @@
+// let SON = +prompt("Son kirit");
+// if (SON == 1) {
+//     alert("Dushanba");
+// } else if (SON == 2) {
+//     alert("Seshanba");
+// } else if (SON == 3) {
+//     alert("Chorshanba");
+// } else if (SON == 4) {
+//     alert("Payshanba");
+// } else if (SON == 5) {
+//     alert("Juma");
+// } else if (SON == 6) {
+//     alert("Shanba");
+// } else if (SON == 7) {
+//     alert("Yakshanba");
+// // }
+// let SON = +prompt("Son kirit");
+// if (SON == 1) {
+//     alert("qish");
+// } else if (SON == 2) {
+//     alert("qish");
+// } else if (SON == 3) {
+//     alert("bahor");
+// } else if (SON == 4) {
+//     alert("bahor");
+// } else if (SON == 5) {
+//     alert("bahor");
+// }
+// else if (SON == 6) {
+//     alert("yoz");
+// }else if (SON == 7) {
+//     alert("yoz");
+// }else if (SON == 8) {
+//     alert("yoz");
+// }else if (SON == 9) {
+//     alert("kuz");
+// }else if (SON == 10) {
+//     alert("kuz");
+// }else if (SON == 11) {
+//     alert("kuz");
+// }else if (SON == 12) {
+//     alert("qish");
+// }
+// let SON = +prompt("Son kirit");
+// if (SON == 1) {
+//     alert("qish yanvar 31 kun");
+// } else if (SON == 2) {
+//     alert("qish fevral 29 kun");
+// } else if (SON == 3) {
+//     alert("bahor mart 31 kun");
+// } else if (SON == 4) {
+//     alert("bahor aprel 30 kun");
+// } else if (SON == 5) {
+//     alert("bahor may 31 kun ");
+// }
+// else if (SON == 6) {
+//     alert("yoz iyun 30 kun");
+// }else if (SON == 7) {
+//     alert("yoz iyul 31 kun ");
+// }else if (SON == 8) {
+//     alert("yoz avgust 31 kun");
+// }else if (SON == 9) {
+//     alert("kuz sentyabr 30 kun");
+// }else if (SON == 10) {
+//     alert("kuz oktyabr 31 kun");
+// }else if (SON == 11) {
+//     alert("kuz noyabr 30 kun");
+// }else if (SON == 12) {
+//     alert("qish dekabr 31 kun");
+// }
+// let A = 5
+// let B = 6
+// let C =   +prompt("Son kirit");
+// if ( C == 1) {
+//     alert(A+B);
+// } else if ( C == 2) {
+//     alert(A-B);
+// } else if ( C == 3) {
+//     alert(A/B);
+// } else if ( C == 4) {
+//     alert(A*B);
+// }
+let A = +prompt("Son kirit");
+let meter = 100
+if ( A == 1) {
+    alert( meter / 10)
+}
+else if ( A == 2) {
+    alert( meter / 1000)
+}
+else if ( A == 3) {
+    alert( meter )
+}
+else if ( A == 4) {
+    alert( meter * 100000)
+}
+else if ( A == 5) {
+    alert( meter * 10000)
+}
